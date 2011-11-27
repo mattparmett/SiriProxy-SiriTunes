bb59025984092c21a37d62e64ec31e889d4a157c
parm289 <parm289@yahoo.com>
2011-11-27 01:35:00 -0500

    fixing put on

--- lib/siriproxy-siritunes.rb
+++ lib/siriproxy-siritunes.rb
@@ -15,6 +15,60 @@ class SiriProxy::Plugin::SiriTunes < SiriProxy::Plugin
     #if you have custom configuration options, process them here!
   end
 
+  listen_for /itunes put on (.*)/i do |name|
+	name = name.split(' ').map {|w| w.capitalize }.join(' ')
+	itunes = WIN32OLE.new('iTunes.Application')
+	library = itunes.LibraryPlaylist
+	tracks = library.Tracks
+	song = tracks.ItemByName(songName)
+	if !song
+		#Song wasn't found.  Check if the user said an artist.
+		artist_tracks = library.Search(songName, 2)
+		if !artist_tracks #artist wasn't found. check if the user said an album.
+			album_tracks = library.Search(songName, 3)
+			if !album_tracks #album wasn't found.  We give up.
+				say "Sorry, I couldn't find " + songName + " in your library."
+				request_completed
+			else
+				songs = []
+				for track in album_tracks
+					songs << track
+				end
+				song = songs[0]
+				if !song
+					#We give up
+					say "Sorry, I couldn't find " + songName + " in your library."
+					request_completed
+				else
+					song.Play
+					say "Playing " + songName + "."
+					request_completed
+				end
+			end
+		end
+		songs = []
+		for track in artist_tracks
+			songs << track
+		end
+		song = songs[0]
+		if !song
+			#this should never happen. let's print a message if it does
+			puts "The artist was found but the song is null."
+			request_completed
+		else
+			songNum = rand(songs.length)
+			song = songs[songNum]
+			song.Play
+			say "Playing " + songName + "."
+			request_completed
+		end
+	else
+		song.Play
+		say "Playing " + songName + "."
+		request_completed
+	end
+  end
+  
   listen_for /itunes (.*)/i do |userAction|
 	userAction = userAction.downcase
 	puts userAction
@@ -78,60 +132,6 @@ class SiriProxy::Plugin::SiriTunes < SiriProxy::Plugin
 		request_completed
 	else
 		say "Sorry, I didn't understand your request."
-		request_completed
-	end
-  end
-  
-  listen_for /itunes put on (.*)/i do |name|
-	name = name.split(' ').map {|w| w.capitalize }.join(' ')
-	itunes = WIN32OLE.new('iTunes.Application')
-	library = itunes.LibraryPlaylist
-	tracks = library.Tracks
-	song = tracks.ItemByName(songName)
-	if !song
-		#Song wasn't found.  Check if the user said an artist.
-		artist_tracks = library.Search(songName, 2)
-		if !artist_tracks #artist wasn't found. check if the user said an album.
-			album_tracks = library.Search(songName, 3)
-			if !album_tracks #album wasn't found.  We give up.
-				say "Sorry, I couldn't find " + songName + " in your library."
-				request_completed
-			else
-				songs = []
-				for track in album_tracks
-					songs << track
-				end
-				song = songs[0]
-				if !song
-					#We give up
-					say "Sorry, I couldn't find " + songName + " in your library."
-					request_completed
-				else
-					song.Play
-					say "Playing " + songName + "."
-					request_completed
-				end
-			end
-		end
-		songs = []
-		for track in artist_tracks
-			songs << track
-		end
-		song = songs[0]
-		if !song
-			#this should never happen. let's print a message if it does
-			puts "The artist was found but the song is null."
-			request_completed
-		else
-			songNum = rand(songs.length)
-			song = songs[songNum]
-			song.Play
-			say "Playing " + songName + "."
-			request_completed
-		end
-	else
-		song.Play
-		say "Playing " + songName + "."
 		request_completed
 	end
   end
