4ef67656a700edf9244844fdf8a8d2b3f3877572
parm289 <parm289@yahoo.com>
2011-11-27 20:46:21 -0500

    tests

--- lib/siriproxy-siritunes.rb
+++ lib/siriproxy-siritunes.rb
@@ -115,19 +115,19 @@ class SiriProxy::Plugin::SiriTunes < SiriProxy::Plugin
 	request_completed
   end
   
-  listen_for (/itunes what's playing/i) do
-	itunes = WIN32OLE.new('iTunes.Application')
-	currentTrack = itunes.CurrentTrack
-	say currentTrack.Name + " by " currentTrack.Artist + " is playing in iTunes."
-	request_completed
-  end
+  # listen_for (/itunes what's playing/i) do
+	# itunes = WIN32OLE.new('iTunes.Application')
+	# currentTrack = itunes.CurrentTrack
+	# say currentTrack.Name + " by " currentTrack.Artist + " is playing in iTunes."
+	# request_completed
+  # end
   
-  listen_for (/i tunes what's playing/i) do
-	itunes = WIN32OLE.new('iTunes.Application')
-	currentTrack = itunes.CurrentTrack
-	say currentTrack.Name + " by " currentTrack.Artist + " is playing in iTunes."
-	request_completed
-  end
+  # listen_for (/i tunes what's playing/i) do
+	# itunes = WIN32OLE.new('iTunes.Application')
+	# currentTrack = itunes.CurrentTrack
+	# say currentTrack.Name + " by " currentTrack.Artist + " is playing in iTunes."
+	# request_completed
+  # end
   
   listen_for /itunes play (.*)/i do |name|
 	if !name or name == "" or name == " "
